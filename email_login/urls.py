522d320c79d48b1c6d082242e20a6561088dbdd7
Tino de Bruijn <tinodb@gmail.com>
2010-12-28 13:52:30 +0100

    Fixed urls, and added 'forgot password'-link to login form

--- email_login/urls.py
+++ email_login/urls.py
@@ -1,9 +1,12 @@
 # We need to override login view, the rest is ok.
 from django.conf.urls.defaults import *
-from django.contrib.auth.urls import urlpatterns
+from django.contrib.auth.urls import urlpatterns as auth_urls
+from forms import EmailAuthenticationForm
 
-urlpatterns += patterns('',
+urlpatterns = patterns('',
     (r'^login/$', 'django.contrib.auth.views.login', 
         { 'template_name' : 'email_login/login.html',
-          'authentication_form' : 'email_login.forms.EmailAuthenticationForm'}),
+          'authentication_form' : EmailAuthenticationForm}),
 )
+
+urlpatterns += auth_urls
