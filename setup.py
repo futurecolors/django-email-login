9121c1722a4f08d250528c3a88b119febb0176a4
Hyungoo Kang <cyluss@gmail.com>
2012-06-15 15:59:48 +0900

    fixed setup script to include template

--- setup.py
+++ setup.py
@@ -1,19 +1,16 @@
 #!/usr/bin/env python
- 
-from distutils.core import setup
+
+from setuptools import setup 
 import email_login
 
 setup(
     name='django-email-login',
     version=email_login.__version__,
-    description='A Django application that enables you to have your users use their email address instead of their username',
-    long_description=open('README.txt').read(),
-    licences='MIT License, see LICENSE.txt',
     author='Tino de Bruijn',
     author_email='tinodb@gmail.com',
     url='http://bitbucket.org/tino/django-email-login',
-    packages=['email_login'],
-    zip_safe=False,
+    description='A Django application that enables you to have your users use their email address instead of their username',
+    long_description=open('README.txt').read(),
     classifiers=[
         'Programming Language :: Python',
         'License :: OSI Approved :: MIT License',
@@ -23,5 +20,10 @@ setup(
         'Framework :: Django',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Libraries :: Python Modules',
-    ]
+    ],
+    license='MIT License',
+    packages=['email_login'],
+    include_package_data=True,
+    zip_safe=False,
 )
+
