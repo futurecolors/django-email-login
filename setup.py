dfd55b59a348ddf0512dc437b8704cd78b47148e
Tino <tinodb@gmail.com>
2012-10-01 20:29:32 +0200

    Merge Hyungoo Kang's fix

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 
-from setuptools import setup 
+from setuptools import setup
 import email_login
 
 setup(
     name='django-email-login',
     version=email_login.__version__,
+    description='A Django application that enables you to have your users use their email address instead of their username',
+    long_description=open('README.txt').read(),
     author='Tino de Bruijn',
     author_email='tinodb@gmail.com',
     url='http://bitbucket.org/tino/django-email-login',
-    description='A Django application that enables you to have your users use their email address instead of their username',
-    long_description=open('README.txt').read(),
+    packages=['email_login'],
+    zip_safe=False,
     classifiers=[
         'Programming Language :: Python',
         'License :: OSI Approved :: MIT License',
@@ -22,8 +24,6 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     license='MIT License',
-    packages=['email_login'],
     include_package_data=True,
-    zip_safe=False,
 )
 
